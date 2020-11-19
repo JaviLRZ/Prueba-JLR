128356caf5604b473631af79809d55ddacce2f26
alvaro-MG <alvaromartin.ciclos@gmail.com>
2020-11-19 13:17:03 +0100

    FicheroCabiado

--- hp04.php
+++ hp04.php
@@ -4,7 +4,7 @@
  </head>
  <body>
 <?php echo '<h1>Harry Porlotes y El Malware de Fuego</h1>'; ?>
-<?php print("<p>Un importante hackaton se celebra en la escuela y el portátil de un compañero de Harry, se avería debido a un ataque de Ransomwaremort, perdiendo así toda la saga Crepúsculo en epub que tenía guardada y las revistas de Marie-Claire de su abuela en PDF.</p>"
+<?php print("<p>Me gusta mucho Harry el Sucio.</p>"
            );?>
  </body>
 </html>
